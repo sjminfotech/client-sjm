5d9143410115cb55c8e79a983e2a89199ff2e5ca
sjminfotech <elegancewear.in@gmail.com>
2025-07-27 13:39:45 +0530

    ceo

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import CEO from "../assets/IMAGES/ceo.png";
+import CEO from "../assets/IMAGES/ceo.jpg";
  import './about.css';
 const About = () => (
   <>
